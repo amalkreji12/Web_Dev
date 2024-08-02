d2554fb406fd25787724a2627368e86e690826f1
Amal K Reji <amalreji45.amal@gmail.com>
2024-08-02 15:47:51 +0530

    Initial Commit

---
+++ practice/web_dev/js_practice/sum.js
@@ -0,0 +1,17 @@
+//sum
+var a=10;
+var b=20;
+var sum=a+b;
+console.log(sum);
+
+// Inputing values from user
+
+var read=require('readline-sync')
+
+// var val=read.question('Enter a number : ');
+// console.log(val);
+
+//condition statement
+
+num1=read.question('Enter a number : ');
+num2=read.question('Enter a number : ');
--- practice/web_dev/sum.js
+++
@@ -1,8 +0,0 @@
-//sum
-var a=10;
-var b=20;
-var sum=a+b;
-console.log(sum);
-
-//condition statement
-
