90a48ef3c18ec758109415b4f18ccd44404e8bb5
Amal K Reji <amalreji45.amal@gmail.com>
2024-08-05 17:21:05 +0530

    express.js example

--- practice/web_dev/js_practice/node/express.js
+++ practice/web_dev/js_practice/node/express.js
@@ -1,17 +1,17 @@
 const express=require('express');
-const path=require('path');
+const path=require('path');    //  Path module provides a way of working with directories and file paths.
 const bodyParser = require('body-parser');
 
 const app=express();
 
 
-app.use(function(req,res,next){                // use is used as a middleware
+app.use(function(req,res,next){                // 'use' is used as a middleware
     console.log('In the middleware'); 
     next();
 })
 
 app.get('/signup',function(req,res){
-    res.sendFile(path.join(__dirname,'signup.html'));
+    res.sendFile(path.join(__dirname,'signup.html'));    //__dirname is used to find current directory
     //res.send('Hello World');
 })
 
