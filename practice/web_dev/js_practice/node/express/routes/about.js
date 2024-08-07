88cb36f33e1b345e432a73ad64c884044fdd866b
Amal K Reji <amalreji45.amal@gmail.com>
2024-08-07 15:38:01 +0530

    express/handlebar

--- practice/web_dev/js_practice/node/express/routes/about.js
+++ practice/web_dev/js_practice/node/express/routes/about.js
@@ -4,7 +4,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',function(req,res,next){
-    res.render('about',{text:'World'});
+
+    const disName=['Apple','Mango','Banana'];
+
+    const person={name:'Orange',comments:{comment:'This is a comment',date:'01-01-2021'}};
+
+    const value={name:'Apple',admin:true};  
+
+    res.render('about',{disName,text:'About Page',person,value});
 });
 
 module.exports=router;
