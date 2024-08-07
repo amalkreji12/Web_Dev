62aba7396c5294adeb89fac3e618e9dd27e9d6ae
Amal K Reji <amalreji45.amal@gmail.com>
2024-08-07 13:54:25 +0530

    Initial Commit

--- practice/web_dev/js_practice/node/express.js
+++ practice/web_dev/js_practice/node/express.js
@@ -1,3 +1,5 @@
+
+
 const express=require('express');
 const path=require('path');    //  Path module provides a way of working with directories and file paths.
 const bodyParser = require('body-parser');
@@ -10,12 +12,14 @@ app.use(function(req,res,next){                // 'use' is used as a middleware
     next();
 })
 
+
 app.get('/signup',function(req,res){
     res.sendFile(path.join(__dirname,'signup.html'));    //__dirname is used to find current directory
     //res.send('Hello World');
 })
 
 app.post('/signup',function(req,res){
+    //console.log(req.body);
     res.send('Sign Up')
 })
 
@@ -23,6 +27,12 @@ app.get('/about',function(req,res){
     res.send('About Page');
 })
 
+
+
+
+
+
+///server creation
 app.listen(3000,function(){
     console.log(__filename);
     console.log('Server Started');
