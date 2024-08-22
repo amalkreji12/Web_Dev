37f469f25007cb3f7d780accbe63347826baa412
Amal K Reji <amalreji45.amal@gmail.com>
2024-08-22 12:49:06 +0530

    added changeProductCount function to increment and decrement cart count

--- practice/web_dev/shopping/helpers/user-helpers.js
+++ practice/web_dev/shopping/helpers/user-helpers.js
@@ -51,7 +51,7 @@ module.exports = {
                 let proExtist = usercart.products.findIndex(product => product.item == proId);
                 console.log(proExtist);
                 if (proExtist != -1) {
-                    db.getDb().collection(collections.CART_COLLECTION).updateOne({ 'products.item': new objectId(proId) },
+                    db.getDb().collection(collections.CART_COLLECTION).updateOne({user:new objectId(userId), 'products.item': new objectId(proId) },
                         {
                             $inc: { 'products.$.quantity': 1 }
                         }
@@ -92,25 +92,25 @@ module.exports = {
                     $match: { user: new objectId(userId) }
                 },
                 {
-                    $unwind:'$products'
+                    $unwind: '$products'
                 },
                 {
-                    $project:{
-                        item:'$products.item',
-                        quantity:'$products.quantity'
+                    $project: {
+                        item: '$products.item',
+                        quantity: '$products.quantity'
                     }
                 },
                 {
-                    $lookup:{
-                        from:collections.PRODUCT_COLLECTION,
-                        localField:'item',
-                        foreignField:'_id',
-                        as:'product'
+                    $lookup: {
+                        from: collections.PRODUCT_COLLECTION,
+                        localField: 'item',
+                        foreignField: '_id',
+                        as: 'product'
                     }
                 },
                 {
-                    $project:{
-                        item:1,quantity:1,product:{$arrayElemAt:['$product',0]}
+                    $project: {
+                        item: 1, quantity: 1, product: { $arrayElemAt: ['$product', 0] }
                     }
                 }
                 // {
@@ -131,9 +131,9 @@ module.exports = {
                 // }
 
             ]).toArray();
-            console.log(cartItems);
-            
-            if (cartItems.length > 0 ) {
+            //console.log(cartItems);
+
+            if (cartItems.length > 0) {
                 //resolve(cartItems[0].cartItems);
                 resolve(cartItems)
             } else {
@@ -151,6 +151,20 @@ module.exports = {
                 count = cart.products.length;
             }
             resolve(count);
+        })
+    },
+
+    changeProductCount(details) {
+        details.count=parseInt(details.count);
+        return new Promise((resolve, reject) => {
+            db.getDb().collection(collections.CART_COLLECTION).updateOne({_id:new objectId(details.cart), 'products.item': new objectId(details.product) },
+                {
+                    $inc: { 'products.$.quantity': details.count }
+                }
+            ).then((response) => {
+                console.log(response);
+                resolve();
+            })
         })
     }
 
